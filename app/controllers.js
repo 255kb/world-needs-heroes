1ad2042c37d340638b1ab213ffef4aa91cc217ec
Guillaume <hi@guillaumemonnet.com>
2016-07-03 14:24:23 +0200

    remove unused post controller

--- app/controllers.js
+++ app/controllers.js
@@ -1,47 +1,6 @@
 'use strict';
 
 angular.module('wnh.controllers', [])
-    .controller('PostCtrl', ['$scope', '$mdDialog', 'Auth', '$firebaseObject', 'Database', function ($scope, $mdDialog, Auth, $firebaseObject, Database) {
-
-
-        /*$scope.showLoginDialog = function (event) {
-         $mdDialog.show({
-         controller: function DialogController($scope, $mdDialog, $firebaseAuth) {
-         $scope.hide = function () {
-         $mdDialog.hide();
-         };
-         $scope.login = function () {
-         Auth.$signInAnonymously().then(function () {
-         $mdDialog.hide();
-         }).catch(function (error) {
-         //TODO display error
-         $scope.error = error;
-         });
-         };
-         },
-         templateUrl: 'loginDialog.html',
-         parent: angular.element(document.body),
-         targetEvent: event,
-         clickOutsideToClose: true
-         })
-         .then(function (answer) {
-         $scope.status = 'You said the information was "' + answer + '".';
-         }, function () {
-         $scope.status = 'You cancelled the dialog.';
-         });
-         };*/
-
-        /*$scope.post = function () {
-         if (Auth.getUser() && $scope.videoLink) {
-         var videoId = $scope.videoLink.match();
-
-         Database.newPost({youtubeId: videoId});
-         } else {
-         $scope.showLoginPopup();
-         }
-         };*/
-    }])
-
     .controller('ToolbarCtrl', ['$scope', '$mdDialog', 'Auth', 'Database', function ($scope, $mdDialog, Auth) {
         var showLoginDialog = function (event) {
             $mdDialog.show({
